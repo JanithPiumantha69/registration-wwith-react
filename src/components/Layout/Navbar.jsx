33fc86ad924999b446e42075386ce5433a23ff87
JanithPiumantha <janithpiumantha69@gmail.com>
2025-10-30 14:24:18 +0530

    dashboard component

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,17 +1,29 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 import './Navbar.css'
 
 export default function Navbar(){
     const {currentUser, logout} = useAuth();
     const navigate = useNavigate();
+
+    //handle user logout
+    async function handleLogout() {
+        try{
+            await logout();
+            navigate('/login');
+        }catch(error){
+            console.error('Failed to login', error);
+        }
+    }
+    
     return (
         <nav className="navbar">
             <div className="nav-container">
                 <Link to="/" className="nav-logo">Registration </Link>
 
-                <din className="nav-menu">
+                <div className="nav-menu">
 
-                </din>
+                </div>
             </div>
         </nav>
     )
